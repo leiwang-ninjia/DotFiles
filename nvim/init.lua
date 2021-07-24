a501c568ff4d1c064d35a19d373b22d372964370
leiwan <wlwl211@163.com>
2021-07-24 12:47:25 +0800

    Neovim support
    Signed-off-by: leiwan <wlwl211@163.com>

---
+++ nvim/init.lua
@@ -0,0 +1,161 @@
+-- Install packer
+local execute = vim.api.nvim_command
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+
+vim.cmd [[packadd packer.nvim]]
+vim.api.nvim_exec([[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]], false)
+
+local packer = require('packer')
+packer.init({
+  git = {
+    clone_timeout = 300, -- 5 mins
+  },
+})
+
+ 	--use {'kdheepak/lazygit.nvim', requires = 'plenary.nvim', cmd = { 'LazyGit', 'LazyGitConfig' },}
+local use = packer.use
+packer.startup(function()
+  use {'wbthomason/packer.nvim', opt = true}
+  use {'nvim-treesitter/nvim-treesitter', opt = true, run = ':TSUpdate',}
+  use {'kyazdani42/nvim-web-devicons', module = 'nvim-web-devicons',}
+  use {'kyazdani42/nvim-tree.lua', requires = 'nvim-web-devicons',
+    cmd = {'NvimTreeClipboard','NvimTreeClose','NvimTreeFindFile','NvimTreeOpen',
+    'NvimTreeRefresh','NvimTreeToggle',},}
+ 	use {'folke/which-key.nvim', event = 'BufWinEnter',}
+  use 'tpope/vim-vinegar'
+  use 'itchyny/lightline.vim'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-commentary'
+  use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+  use 'joshdick/onedark.vim'
+  use { 'lukas-reineke/indent-blankline.nvim', }
+  use 'sheerun/vim-polyglot'
+  use 'lewis6991/gitsigns.nvim'
+  use 'neovim/nvim-lspconfig'
+  use {'hrsh7th/nvim-compe', config=[[require('nvim-compe')]],}
+end)
+
+-- Disable some built-in plugins we don't want
+local disabled_built_ins = {
+  'gzip',
+  'man',
+  'matchit',
+  'matchparen',
+  'shada_plugin',
+  'tarPlugin',
+  'tar',
+  'zipPlugin',
+  'zip',
+  'netrwPlugin',
+}
+
+vim.o.inccommand = "nosplit"
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.wo.number = true
+
+--Do not save when switching buffers
+vim.o.hidden = true
+vim.o.mouse = "a"
+vim.o.timeoutlen = 500
+vim.o.breakindent = true
+
+--Save undo history
+vim.cmd[[set undofile]]
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn="yes"
+
+--Set colorscheme (order is important here)
+vim.o.termguicolors = true
+vim.g.onedark_terminal_italics = 2
+vim.cmd[[colorscheme onedark]]
+
+--Remap space as leader key
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent=true})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap=true, expr = true, silent = true})
+vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", {noremap= true, expr = true, silent = true})
+
+--Remap escape to leave terminal mode
+vim.api.nvim_exec([[
+  augroup Terminal
+    autocmd!
+    au TermOpen * tnoremap <buffer> <Esc> <c-\><c-n>
+    au TermOpen * set nonu
+  augroup end
+]], false)
+
+--Add map to enter paste mode
+vim.o.pastetoggle="<F3>"
+
+--Map blankline
+vim.g.indent_blankline_char = "┊"
+vim.g.indent_blankline_filetype_exclude = { 'help' }
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile', 'packer'}
+vim.g.indent_blankline_char_highlight = 'LineNr'
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-u>"] = false,
+        ["<C-d>"] = false,
+      },
+    },
+    generic_sorter =  require'telescope.sorters'.get_fzy_sorter,
+    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
+  }
+}
+--Add leader shortcuts
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>bb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ta', [[<cmd>lua require('telescope.builtin').tags()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fr', [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>*', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>/', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>tb', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]], { noremap = true, silent = true})
+
+-- Change preview window location
+vim.g.splitbelow = true
+
+-- Highlight on yank
+vim.api.nvim_exec([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]], false)
+
+-- Y yank until the end of line
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true})
+--
+-- Set completeopt to have a better completion experience
+vim.o.completeopt="menuone,noinsert,noselect"
+
