1eca4bc7716ccca13cd51162398dc3945f2728e1
Lei wang <quic_leiwan@quicinc.com>
2022-02-23 23:49:56 +0800

    add install

--- misc/install.sh
+++ misc/install.sh
@@ -1,4 +1,8 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-#rust
-#curl https://sh.rustup.rs -sSf | sh
+mkdir -p /local/mnt/workspace/leiwan/nvim_plugins/
+mkdir -p /local/mnt/workspace/leiwan/bin
+curl -o bat.tar.gz -L https://github.com/sharkdp/bat/releases/download/v0.19.0/bat-v0.19.0-aarch64-unknown-linux-gnu.tar.gz
+tar -xvzf bat.tar.gz
+curl -o ripgrep.tar.gz -L https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+tar -xvzf ripgrep.tar.gz
