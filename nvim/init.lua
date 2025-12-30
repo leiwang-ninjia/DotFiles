c1ccf59f2cb2cb3be0d8b621528298abe866eb55
Lei wang <leiwan@qti.qualcomm.com>
2025-12-30 13:00:54 +0800

    add support for nvim 0.12

--- nvim/init.lua
+++ nvim/init.lua
@@ -388,10 +388,11 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require'lspconfig'.clangd.setup{
+
+vim.lsp.config("clangd", {
   on_attach = on_attach,
   flags = lsp_flags,
-}
+})
 
 -- Change preview window location
 vim.g.splitbelow = true
